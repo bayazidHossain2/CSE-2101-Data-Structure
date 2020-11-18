df61e1a6dc66e82b61ada11fea4a8099c475ec5a
bayazidHossain2 <67774055+bayazidHossain2@users.noreply.github.com>
2020-11-18 12:47:57 +0600

    Update 02Linked List function.cpp

--- 02Linked List function.cpp
+++ 02Linked List function.cpp
@@ -5,24 +5,45 @@ public:
     int data;
     Node *next;
 };
+Node *head,*endp;
 Node *creat_node(int item,Node *next){
-    Node *n_node=(Node*) new Node;
-    if(n_node == NULL){
+    Node *new_node=(Node*) new Node;
+    if(new_node == NULL){
         cout<<"Error!! \n Could Not create new node"<<endl;
         exit(1);
     }
-    n_node->data = item;
-    n_node->next = next;
-    return n_node;
+    new_node->data = item;
+    new_node->next = next;
+    return new_node;
 }
-Node *prepend(Node *head,int item){
+void prepend(int item){
     Node *new_node = creat_node(item,head);
-    return new_node;
+    head = new_node;
 }
-Node *append(Node *endp,int item){
-    Node *new_node = creat_node(item,NULL);
+void append(int item){
+    Node *new_node = creat_node(item,nullptr);
     (*endp).next = new_node;
-    return new_node;
+    endp = new_node;
+}
+void remove_node(Node *node){
+    if(node == head){
+        head = node->next;
+        delete(node);
+        return;
+    }
+    Node *current_node = head;
+    while(current_node != nullptr){
+        if(current_node->next == node){
+            break;
+        }current_node = current_node->next;
+    }
+    if(node == endp){
+        endp = current_node;
+        delete(node);
+        return;
+    }
+    current_node->next = node->next;
+    delete(node);
 }
 void print(Node *head){
     cout<<(*head).data<<' ';
@@ -32,21 +53,39 @@ void print(Node *head){
     }cout<<endl;
 }
 int main(){
-    Node *n ,*head,*endp;
+    Node *n;
     cout<<"Use of prepend function: "<<endl;
     n = creat_node(10,NULL);
     head = n;
     endp = n;
-    head = prepend(head,20);
-    head = prepend(head,30);
-    head = prepend(head,40);
+    prepend(20);
+    prepend(30);
+    prepend(40);
     print(head);
     cout<<"Use of append function: "<<endl;
-    endp = append(endp,1);
-    endp = append(endp,100);
+    append(1);
+    append(100);
     print(head);
     cout<<"Again prepend : "<<endl;
-    head = prepend(head,50);
+    prepend(50);
+    print(head);
+    cout<<"Delete some node"<<endl;
+    remove_node(head);
+    cout<<head->data<<endl;
+    cout<<"After delete first node : ";
+    print(head);
+
+    Node *any_node = head->next;
+    any_node = any_node->next;
+    cout<<"Delete "<<any_node->data<<" from the data set : "<<endl;
+    remove_node(any_node);
+    print(head);
+    remove_node(endp);
+    cout<<"After deleting last element : ";
+    print(head);
+    cout<<"Again append 11 and prepend 13 : ";
+    append(11);
+    prepend(13);
     print(head);
     return 0;
 }

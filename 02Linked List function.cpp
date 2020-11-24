92fab6dec03d77332b41b9756e7879e22bfbf284
bayazidHossain2 <67774055+bayazidHossain2@users.noreply.github.com>
2020-11-24 13:16:06 +0600

    Update 02Linked List function.cpp

--- 02Linked List function.cpp
+++ 02Linked List function.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
+
 class Node{
 public:
     int data;
     Node *next;
 };
 Node *head,*endp;
+///Insertion
 Node *creat_node(int item,Node *next){
     Node *new_node=(Node*) new Node;
     if(new_node == NULL){
@@ -25,6 +27,11 @@ void append(int item){
     (*endp).next = new_node;
     endp = new_node;
 }
+void insert(Node *node,int item){
+    Node *new_node = creat_node(item,node->next);
+    node->next = new_node;
+}
+///Deletion
 void remove_node(Node *node){
     if(node == head){
         head = node->next;
@@ -45,10 +52,6 @@ void remove_node(Node *node){
     current_node->next = node->next;
     delete(node);
 }
-void insert(Node *node,int item){
-    Node *new_node = creat_node(item,node->next);
-    node->next = new_node;
-}
 void remove_item(int item){
     Node *current_node = head;
     while(current_node != nullptr){
@@ -61,6 +64,15 @@ void remove_item(int item){
         remove_node(current_node);
     }
 }
+void removeall_item(int item){
+    Node *current_node = head;
+    while(current_node != nullptr){
+        if(current_node->data == item){
+            remove_node(current_node);
+        }current_node = current_node->next;
+    }
+}
+///Traversing
 void print(){
     Node *current_node = head;
     cout<<current_node->data<<' ';
@@ -69,6 +81,35 @@ void print(){
         cout<<(*current_node).data<<' ';
     }cout<<endl;
 }
+int list_size(){
+    int lsize = 0;
+    Node *current_node = head;
+    while(current_node != nullptr){
+        lsize++;
+        current_node = current_node->next;
+    }return lsize;
+}
+///Searching
+Node *find_item(int item){
+    Node *current_node = head;
+    while(current_node != nullptr){
+        if(current_node->data == item){
+            return current_node;
+        }current_node = current_node->next;
+    }return nullptr;
+}
+bool is_present(int item){
+    if(find_item(item) == nullptr) return false;
+    return true;
+}
+///Sorting
+void swap(Node *n1,Node *n2){
+    Node *temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
+
+
 int main(){
     Node *n;
     cout<<"Use of prepend function: "<<endl;
@@ -90,6 +131,7 @@ int main(){
     remove_node(head);
     cout<<"After delete first node : ";
     print();
+    cout<<"List size : "<<list_size()<<endl;
 
     Node *any_node = head->next;
     any_node = any_node->next;
@@ -123,5 +165,23 @@ int main(){
     cout<<"Insert item after head : ";
     insert(head,34);
     print();
+    append(34);
+    prepend(34);
+    print();
+    removeall_item(34);
+    print();
+    cout<<"Again append 11 and prepend 99 : ";
+    append(11);
+    prepend(99);
+    print();
+    cout<<"List size : "<<list_size()<<endl;
+
+    any_node = find_item(50);
+    if(any_node != nullptr){
+        cout<<any_node->data<<endl;
+    }else cout<<"Not found."<<endl;
+    if(is_present(1)) cout<<"1 ia present."<<endl;
+    else cout<<"1 is not present."<<endl;
+
     return 0;
 }
